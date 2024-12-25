c80be06813a0db5c0d6fa067ee518308421f8817
Jeff Mallozzi <jeffmallozzi@gmail.com>
2024-12-25 14:49:46 -0500

    Day 9 part 2

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,7 +1,11 @@
-use itertools::{enumerate, FilterMapOk};
+use itertools::{enumerate, FilterMapOk, Itertools};
+use regex::bytes::CaptureLocations;
 
 use crate::{Solution, SolutionPair};
-use std::{borrow::Borrow, clone, fs::read_to_string, str::SplitAsciiWhitespace};
+use std::{
+    arch::x86_64::_mm256_undefined_pd, borrow::Borrow, clone, collections::HashMap,
+    fs::read_to_string, iter::FilterMap, num::NonZeroUsize, str::SplitAsciiWhitespace,
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -11,63 +15,90 @@ pub fn solve() -> SolutionPair {
     let input = read_to_string(input_file).expect("Hey!");
 
     let sol1: i64 = solution1(&input);
-    let sol2: i64 = solution2(&input);
+    let sol2: usize = solution2(&input);
 
     (Solution::from(sol1), Solution::from(sol2))
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 struct File {
-    id: i64,
-    location: i64,
-    length: i64,
+    id: usize,
+    length: usize,
 }
 
 #[derive(Debug, Clone)]
 struct Disk {
-    sectors: Vec<i64>,
-    files: Vec<File>,
+    sectors: Vec<Option<usize>>,
+    files: HashMap<usize, File>,
+    space: HashMap<usize, usize>,
 }
 
 impl Disk {
     fn new() -> Self {
         Disk {
             sectors: Vec::new(),
-            files: Vec::new(),
+            files: HashMap::new(),
+            space: HashMap::new(),
         }
     }
 
-    fn add(&mut self, mut file: File) {
+    fn add_file(&mut self, mut file: File) {
         let location = self.sectors.len();
-        file.location = (location as i64);
         for sector in 0..file.length {
-            self.sectors.push(file.id);
+            self.sectors.push(Some(file.id));
         }
-        self.files.push(file);
+        self.files.insert(location, file);
     }
 
-    fn find_space(&self, size: i64) -> Option<i64> {
-        for (i, sector) in enumerate(self.sectors.clone()) {
-            if sector < 0 && i < self.sectors.len() - (size as usize) {
-                if self.sectors[i..i + (size as usize)].iter().all(|x| x < &0) {
-                    return Some((i as i64));
+    fn add_space(&mut self, length: usize) {
+        let location = self.sectors.len();
+        self.space.insert(location, length);
+        for sector in 0..length {
+            self.sectors.push(None);
+        }
+    }
+
+    fn find_space(&mut self, size: usize) -> Option<usize> {
+        self.space
+            .iter()
+            .filter(|&(_, v)| *v >= size)
+            .min_by_key(|&(k, _)| k)
+            .map(|(&k, _)| k)
+    }
+
+    fn get_file(&mut self, location: usize) -> Option<File> {
+        let file = self.files.get(&location);
+        match file {
+            None => None,
+            Some(f) => Some(*f),
+        }
+    }
+
+    fn move_file(&mut self, location: usize) {
+        let file = self.get_file(location);
+        match file {
+            None => return,
+            Some(f) => match self.find_space(f.length) {
+                None => return,
+                Some(new_location) => {
+                    let space_length = *self.space.get(&new_location).unwrap();
+                    if new_location >= location {
+                        return;
+                    }
+                    for i in 0..f.length {
+                        self.sectors[new_location + i] = Some(f.id);
+                        self.sectors[location + i] = None;
+                    }
+                    self.files.remove(&location);
+                    self.files.insert(new_location, f);
+                    self.space.remove(&new_location);
+                    self.space.insert(location, f.length);
+                    if space_length > f.length {
+                        self.space
+                            .insert(new_location + f.length, space_length - f.length);
+                    }
                 }
-            }
-        }
-        None
-    }
-
-    fn move_file(&mut self, mut file_index: i64, mut location: i64) {
-        let mut old_index = self.files[(file_index as usize)].location;
-        let id = self.files[(file_index as usize)].id;
-        let length = self.files[(file_index as usize)].length;
-
-        self.files[(file_index as usize)].location = location;
-        for _ in 0..length {
-            self.sectors[(file_index as usize)] = id;
-            self.sectors[(old_index as usize)] = -1;
-            old_index += 1;
-            file_index += 1;
+            },
         }
     }
 }
@@ -113,35 +144,44 @@ fn solution1(input: &str) -> i64 {
     checksum
 }
 
-fn solution2(input: &str) -> i64 {
+fn solution2(input: &str) -> usize {
     let mut disk = Disk::new();
-    let mut file_id: i64 = 0;
-    let mut indicator: i64 = 1;
-    let mut checksum: i64 = 0;
+    let mut file_id: usize = 0;
+    let mut indicator: i8 = 1;
+    let mut checksum: usize = 0;
 
     for number in input.trim().chars() {
-        disk.add(File {
-            id: file_id * indicator,
-            location: 0,
-            length: (number.to_digit(10).expect("Hey!") as i64),
-        });
+        let length = (number.to_digit(10).unwrap() as usize);
         if indicator > 0 {
+            disk.add_file(File {
+                id: file_id,
+                length,
+            });
             file_id += 1;
+        } else {
+            disk.add_space(length);
         }
         indicator *= -1;
-        //file_id += 1;
     }
 
-    let files = disk.files.as_mut_slice();
+    let mut files: Vec<usize> = disk
+        .files
+        .iter()
+        .map(|(location, file)| *location)
+        .sorted()
+        .collect();
     files.reverse();
 
-    for file in files {
-        let free_space = disk.find_space(file.length);
-        match free_space {
-            Some(t) => println!("Free space at sector: {}", t),
+    for location in files {
+        disk.move_file(location);
+    }
+
+    for (i, num) in enumerate(disk.sectors) {
+        match num {
+            Some(n) => checksum += n * i,
             None => continue,
         }
     }
 
-    0
+    checksum
 }
