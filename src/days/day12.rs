24b4bfed8fa937f12342d5ae251648b664b71a04
Jeff Mallozzi <jeffmallozzi@gmail.com>
2025-02-21 13:18:30 -0500

    Check point 2 day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,8 +1,10 @@
 use itertools::{enumerate, Product};
 
 use crate::{Solution, SolutionPair};
+use std::arch::x86_64::_MM_FROUND_TO_ZERO;
 use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
+use std::ops::{Index, IndexMut};
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -50,10 +52,52 @@ impl Region {
     fn price(self) -> usize {
         self.area * self.perimeter
     }
+
+    fn grow(mut self, plots: &mut Vec<Plot>) -> Self {
+        let mut found: bool = true;
+
+        while found {
+            found = false;
+            for (i plot) in enumerate(plots) {
+                if plot.crop == self.crop {
+                    let neighborCount = 0;
+                    for location in self.locations {
+                        if neighbors(location, plot.location) {
+                            neighborCount += 1;
+                        }
+                    }
+                    if neighborCount > 0 {
+                        self.area += 1;
+                        self.perimeter += (4 - (2*neighborCount));
+                        plots.swap_remove()
+                    }
+                }
+            }
+        }
+        self
+    }
 }
 
 fn solution1(input: &str) -> usize {
     let mut regions: Vec<Region> = Vec::new();
+
+    let mut plots: Vec<Plot> = Vec::new();
+
+    for (y, line) in enumerate(input.lines()) {
+        for (x, crop) in enumerate(line.chars()) {
+            plots.push(Plot {
+                location: Location { x, y },
+                crop,
+            });
+        }
+    }
+
+    while !plots.is_empty() {
+        let plot = plots.pop().unwrap();
+        let mut region = Region::new(plot);
+        let region = region.grow(&mut plots);
+        regions.push(region);
+    }
 
     regions.into_iter().map(|r| r.price()).sum()
 }
