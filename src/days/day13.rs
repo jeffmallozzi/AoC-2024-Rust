cc2b4625282dbe72c93dd3a39a672909b953394b
Jeff Mallozzi <jeffmallozzi@gmail.com>
2025-04-21 22:21:24 -0400

    Day 13 checkpoint

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -5,8 +5,19 @@ use std::fs::read_to_string;
 
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    let sol1: u64 = 0;
+    let input_file = "input/day13".to_string();
+    let input = read_to_string(input_file).expect("bad path");
+
+    let sol1: usize = solution1(&input);
     let sol2: u64 = 0;
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn solution1(input: &str) -> usize {
+    for line in input.lines() {
+        println!("{}", line);
+    }
+
+    0
 }
